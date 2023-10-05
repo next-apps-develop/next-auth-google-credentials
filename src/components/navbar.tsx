bf814af43d8d826e46e4801819489128cd16a31c
henryGustav <henr_gustavo18@hotmail.com>
2023-10-05 11:02:23 -0500

    adding next auth with dashboard route protected

---
+++ src/components/navbar.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  const { data: session } = useSession()
+
+  return (
+    <nav className='bg-slate-900 flex justify-between px-24 text-white items-center py-3'>
+      <Link href={'/'}>
+        <h1>Next google</h1>
+      </Link>
+      {session?.user ? (
+        <div className='flex gap-x-2 items-center'>
+          <Link href={'/dashboard'}>Dashboard</Link>
+          <p>{session.user.name}</p>
+          <p>{session.user.email}</p>
+          {/* <Image
+            src={session?.user?.image || ''}
+            alt='image'
+            width={50}
+            height={50}
+          ></Image> */}
+          {/* <img src={session.user.image} alt='' /> */}
+
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={session?.user?.image || ''}
+            alt=''
+            width={50}
+            className='rounded-full'
+          />
+
+          <button
+            className='bg-sky-400 px-3 py-2 rounded'
+            onClick={async () => {
+              await signOut({ callbackUrl: '/' })
+            }}
+          >
+            Log out
+          </button>
+        </div>
+      ) : (
+        <button
+          className='bg-sky-400 px-3 py-2 rounded'
+          onClick={() => signIn()}
+        >
+          Sign in
+        </button>
+      )}
+    </nav>
+  )
+}
+
+export default Navbar
